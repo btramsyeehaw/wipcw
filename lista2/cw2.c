49c9b2253238eb8d58334539d23199f6dfafee00
btramsyeehaw <51712687+btramsyeehaw@users.noreply.github.com>
2021-10-30 14:34:35 +0200

    Create cw2.c

---
+++ lista2/cw2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n;
+    printf("Wpisz numer: ");
+    scanf("%d",&n);
+    int p = 0;
+    int i = 1;
+    int pierwszosc = 1;
+    while (p<n){
+        i++;
+        for (int j=2;j<=sqrt(i); j++){
+            if (i%j==0){
+                pierwszosc = 0;
+                break;
+            }
+        }
+        if (pierwszosc == 1){
+            p++;
+            printf("%d\n",i);
+        }
+        pierwszosc = 1;
+    }
+}
